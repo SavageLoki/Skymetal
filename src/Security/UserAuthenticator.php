057f065de653dfbc12d9f4a4c5003c4c3f918abb
Joanna <joanna.reiss@etud.u-picardie.fr>
2021-02-01 11:21:55 +0100

    Changed route for dashboard

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -99,7 +99,7 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
         if (in_array('ROLE_BLOGGER', $roles)) {
-            return new RedirectResponse($this->urlGenerator->generate('homepage'));
+            return new RedirectResponse($this->urlGenerator->generate('dashboard'));
         } else {
             return new RedirectResponse($this->urlGenerator->generate('last_article'));
         }
