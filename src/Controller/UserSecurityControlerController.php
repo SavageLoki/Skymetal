c16728693ecbc855a490c57589de1a2f85780b08
Joanna <joanna.reiss@etud.u-picardie.fr>
2021-02-04 10:30:14 +0100

    Fixes logout

--- src/Controller/UserSecurityControlerController.php
+++ src/Controller/UserSecurityControlerController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserSecurityControlerController extends AbstractController
@@ -31,8 +34,19 @@ class UserSecurityControlerController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    public function logout()
+    public function logout(Request $request, TokenStorageInterface $token, EntityManagerInterface $manager):Response
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        $user = $token->getToken()->getUser();
+
+        if (null !== $user) {
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        $session = $request->getSession();
+        $session->invalidate();
+        $token->setToken(null);
+
+        return $this->redirectToRoute('app_user_home');
     }
 }
